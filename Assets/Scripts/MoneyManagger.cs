222dadd7337a4055288defd5959ee7c23e427a82
Richárd Mészáros <meszarosricsi01@gmail.com>
2019-09-17 00:12:05 +0200

    Initial Commit

---
+++ Assets/Scripts/MoneyManagger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+namespace Clicker.Core
+{
+    public class MoneyManagger : MonoBehaviour
+    {
+        public BigInteger totalMoney { get; private set; } = 0;
+        public float moneyMultiplier { get; private set; } = 1f;
+
+        public void AddToMoney(int amount)
+        {
+            totalMoney += amount;
+        }
+
+        /// <summary>
+        /// Returns TRUE and subtracts money if it is possible, returns FALSE if it's not.
+        /// </summary>
+        public bool SubtractFromMoney(int amount)
+        {
+            if (amount > totalMoney)    return false;
+            else {
+                totalMoney -= amount;
+                return true;
+            }
+
+        }
+
+        public void AddToMoneyMultiplier(float amount)
+        {
+            moneyMultiplier += amount;
+        }
+
+        /// <summary>
+        /// Returns TRUE and subtracts multiplier if it is possible, returns FALSE if it's not.
+        /// </summary>
+        public bool SubtractFromMoneyMultiplier(float amount)
+        {
+            if (amount > moneyMultiplier)   return false;
+            else {
+                moneyMultiplier -= amount;
+                return true;
+            }
+        }
+    }    
+}
---
+++ Assets/Scripts/TextOutput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Clicker.Core;
+
+namespace Clicker.UI
+{
+    public class TextOutput : MonoBehaviour
+    {
+        [SerializeField] Text moneyText;
+        MoneyManagger moneyManagger;
+
+        private void Start() {
+            CacheComponents();
+        }
+
+        private void CacheComponents()
+        {
+            moneyManagger = GetComponent<MoneyManagger>();
+        }
+
+        public void UpdateMoneyText()
+        {
+            moneyText.text = moneyManagger.totalMoney.ToString();
+        }
+    }    
+}
